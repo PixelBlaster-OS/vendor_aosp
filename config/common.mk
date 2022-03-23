c9bcd6e96403383062fc4dd1536e69f73ee17d47
jhenrique09 <jhsv09@gmail.com>
2022-03-23 23:26:49 +0530

    vendor: Enable FaceUnlock

--- config/common.mk
+++ config/common.mk
@@ -112,6 +112,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
 
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Extra tools in PixelBlaster
 PRODUCT_PACKAGES += \
     7z \
